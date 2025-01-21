a91a0109f3686c9bf9195b81e1f490e9dd648119
zaknafien99 <67056163+zaknafien99@users.noreply.github.com>
2025-01-21 22:54:56 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember()
+{
+    var member = new Object();
+    member.name = "John";
+    member.age = 30;
+    member.skills = ["JavaScript", "HTML", "CSS"];
+    return member;
+}
